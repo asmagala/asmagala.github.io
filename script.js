55c435796ac7ada145fd0842582999dd23bbe2a6
asmagala <58421854+asmagala@users.noreply.github.com>
2022-01-09 14:27:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'http://localhost:8080/v1/task/';
+  const apiRoot = 'https://crud-smagaar.herokuapp.com/v1/task/';
   const trelloApiRoot = 'https://crud-smagaar.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
